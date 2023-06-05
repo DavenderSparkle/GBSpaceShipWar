6fbe403413c61161017cda599302d5adbb3c545b
DavenderSparkle <davidwindows-13@hotmail.com>
2023-06-05 13:17:38 +0200

    player and enemy are shown

--- compiler.bat
+++ compiler.bat
@@ -1,9 +1,13 @@
 mkdir result
 
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o result\maptiles.o sprites\maptiles.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o result\chars.o sprites\chars.c
 
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o result\map.o background\map.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o result\mapObjs.o background\mapObjs.c
 
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o result\main.o main.c
 
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wm-yn"ShipWar" -o ShipWar.gb result\map.o result\maptiles.o result\main.o
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wm-yn"ShipWar" -o ShipWar.gb result\main.o ^
+result\map.o result\mapObjs.o ^
+result\chars.o result\maptiles.o 
--- main.c
+++ main.c
@@ -4,12 +4,58 @@
 #include "./background/map.h"
 #include "./background/mapObjs.h"
 #include "./sprites/maptiles.h"
+#include "./sprites/chars.h"
+
+#include "gameCharacter.c"
+
+unsigned int i;
+
+struct gameCharacter player;
+struct gameCharacter enemy;
+
+void setupPlayer()
+{
+    player.x = ((i % mapWidth)<<3) + 8;
+    player.y = ((i / mapWidth)<<3) + 16;
+    player.width = 8;
+    player.height = 8;
+    
+    set_sprite_tile(0,0);
+}
+
+void setupEnemy()
+{
+    enemy.x = ((i % mapWidth)<<3) + 8;
+    enemy.y = ((i / mapWidth)<<3) + 16;
+    enemy.width = 8;
+    enemy.height = 8;
+    
+    set_sprite_tile(1,1);
+}
+
+
+void setupObjts()
+{
+    for(i = 0; i<(mapWidth*mapHeight); i++)
+    {
+        if(mapObjs[i] == 3) {setupPlayer();}
+        if(mapObjs[i] == 4) {setupEnemy();}
+    }
+}
 
 void main()
 {
-    set_bkg_data(0,5,maptiles);
-    set_bkg_tiles(0,0, mapWidth, mapHeight, mapPLN0);
+    set_sprite_data(0,4,chars);
 
+    set_bkg_data(0,5,maptiles);
+    set_bkg_tiles(0,0, mapWidth, mapHeight, map);
+
+    setupObjts();
+
+    move_sprite(0, player.x, player.y);
+    move_sprite(1, enemy.x, enemy.y);
+
+    SHOW_SPRITES;
     SHOW_BKG;
     DISPLAY_ON;
 }
